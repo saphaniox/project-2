1d8d70b1434ba53a8382a198d750511ee9a16663
saphaniox <muganzas80@gmail.com>
2025-06-21 16:00:27 +0300

    code

--- my-app/src/api/api.js
+++ my-app/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://user-data-ci61.onrender.com/user",
+  baseURL: "https://datatabase-test.onrender.com",
 });
-
+// https://datatabase-test.onrender.com
+// https://user-data-ci61.onrender.com/user
 export default API;
